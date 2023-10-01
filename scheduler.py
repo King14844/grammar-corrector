a27df24493601e6f601487f49915e574c3608c20
King14844 <91273036+King14844@users.noreply.github.com>
2023-10-02 00:34:51 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,7 @@ import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=60)
+@sched.scheduled_job('interval', minutes=1)
 def job():
     command = "python3 -m spacy download en"
     subprocess.call(command, shell=True)
