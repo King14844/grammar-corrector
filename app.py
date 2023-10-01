1d18728c7ee9320aa9d69e658f9d9ee59386d2e8
King14844 <91273036+King14844@users.noreply.github.com>
2023-10-01 23:27:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from gramformer import Gramformer
+from autocorrect import Speller
+
+app = Flask(__name__)
+
+def process(text):
+    gf = Gramformer(models=1, use_gpu=False)
+    spell = Speller()
+    corrected = spell(text)
+    ans = ""
+    res = gf.correct(corrected)
+    for ele in res:
+        ans += ele
+    return ans
+
+@app.route('/', methods=["POST"])
+def post():
+    data = request.get_json()
+    res = process(data["text"])
+    return res
+
+if __name__ == '__main__':
+    app.run()
